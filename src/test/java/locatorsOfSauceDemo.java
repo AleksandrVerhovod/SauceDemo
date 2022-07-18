371af8ea780553321934a18da3b910a2311db824
Aleksandr <verhovod.aleksandr@gmail.com>
2022-07-18 13:09:08 +0300

    updating locators

--- src/test/java/locatorsOfSauceDemo.java
+++ src/test/java/locatorsOfSauceDemo.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 public class locatorsOfSauceDemo {
 WebDriver driver;
 WebElement className = driver.findElement(By.className("input_error"));
